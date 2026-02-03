d95716d9eeb226e1f729df653b12b126bf12f9f5
theboy_boy - - $0R3N <138262799+theboy-boy@users.noreply.github.com>
2026-02-03 10:32:00 -0600

    RailCar.java

--- 102-Train/RailCar.java
+++ 102-Train/RailCar.java
@@ -2,5 +2,22 @@ import javax.swing.*;
 import java.awt.*;              
 
 public class RailCar{
+    protected Color color;
+    protected int xCord;
+
+    public RailCar(Color color, int xCord) {
+        this.color = color;
+        this.xCord = xCord;
+    }
+    public void drawCar(Graphics g){
+        g.setColor(color);
+        g.fillRect(xCord+130, 200, 190, 100);
+        g.setColor(Color.black);
+        g.fillOval(xCord+125, 275, 50, 50);
+        g.fillOval(xCord+275, 275, 50, 50);
+
+    }
+
 
 }
+
